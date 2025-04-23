e4e75b03abb51b29f52fe56bf0222418f8b25c29
alumbwe1 <alookscodes@gmail.com>
2025-04-23 20:45:54 +0200

    Added payment models to admin #Remaning with template rendering

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from payments import models
 
 # Register your models here.
+admin.site.register(models.BankAccountDetails)
+admin.site.register(models.CardDetails)
+admin.site.register(models.Settlement)
+admin.site.register(models.MobileMoneyDetails)
+admin.site.register(models.Transaction)
