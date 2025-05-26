372c583a87e8162fb5777309667197fc2c746b17
alumbwe1 <alookscodes@gmail.com>
2025-05-26 18:18:21 +0200

    added setttings.staic url

--- api/urls.py
+++ api/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
 # This allows serving media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
