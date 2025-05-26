5aa6a673bc5833e5d4c6376b594f9c3d5cedfbc4
alumbwe1 <alookscodes@gmail.com>
2025-05-26 18:17:18 +0200

    fixed favicon.ico error

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
     path('api/v1.0/admin/', admin.site.urls),
     path('api/v1.0/cart/', include('cart.urls')),
     path('accounts/', include('allauth.urls')),
