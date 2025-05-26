0f9c5cd2fc56859bde99fd3fe29f290425114562
alumbwe1 <alookscodes@gmail.com>
2025-05-26 23:15:48 +0200

    removed whitenoise

--- api/settings.py
+++ api/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver',
+
 
     # Third-party apps
     'rest_framework',
